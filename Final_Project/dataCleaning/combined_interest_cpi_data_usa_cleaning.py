f6085c40d8f2bc911e2a076be998058bf1871624
Andrew Sullivan <drewsullivan2002@gmail.com>
2025-05-21 14:32:48 -0400

    Data Analytics Final: Predicting Future Inflation

---
+++ Final_Project/dataCleaning/combined_interest_cpi_data_usa_cleaning.py
@@ -0,0 +1,14 @@
+# general imports
+import interest_rate_data_usa_cleaning as interest
+import total_cpi_per_year_usa_cleaning as cpi
+
+# cleaned cpi and interest rate data (dictionaries) from 1954 to 2017
+cleaned_interest_rate_data_1954_to_2017 = interest.get_cleaned_interest_rate_data()
+cleaned_inflation_year_over_year_data_1954_to_2017 = cpi.get_cleaned_cpi_data()
+
+# create a dictionary for the combined clean interest rate and cpi data from 1954 to 2017
+combined_cleaned_interest_inflation_data_1954_to_2017 = {}
+
+# populate combined clean interest and cpi data dictionary from 1954 to 2017
+for key in cleaned_inflation_year_over_year_data_1954_to_2017:
+    combined_cleaned_interest_inflation_data_1954_to_2017[key] = [cleaned_interest_rate_data_1954_to_2017[key], cleaned_inflation_year_over_year_data_1954_to_2017[key]]
